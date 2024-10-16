c059224ecc65f74c3167e8c1c01a8afacca697bd
RishavGaur <rishav021gaur@gmail.com>
2024-10-16 14:06:02 +0530

    Working on World Bank

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>World Bank Group - International Development, Poverty, & Sustainability</title>
+    <link rel="shortcut icon" href="static/img/logos/wbg-favicon.png" type="image/png"/>
+    <link rel="stylesheet" href="static/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+<body>
+    <section id="navigation-bar">
+        <div class="container">
+            <div id="first-nav">
+                <div></div>
+                <div class="first-child">
+                    <a href="#">NEWS</a><span style="color:dimgrey;">|</span>   
+                    <a href="#">ENGLISH</a>
+                </div>
+            </div>
+            <div id="second-nav">
+                <img src="static/img/logos/wbglogo.svg" alt="World Bank Group">
+                <div class="second-child">
+                    <a href="#">WHO WE ARE</a>
+                    <a href="#">WHAT WE DO</a>
+                    <a href="#">WHERE WE WORK</a>
+                    <a href="#">UNDERSTANDING POVERTY</a>
+                    <a href="#">WORK WITH US</a>
+                    <img src="https://www.worldbank.org/content/dam/sites/corporate/img/global/wblive-logo-ENG-short_white2.png" alt="">
+                    <i class="bi bi-search"></i>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="static/js/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
