2722860cc151507ac5cb83d67e2a47adebdb7f77
RishavGaur <rishav021gaur@gmail.com>
2024-10-17 13:04:22 +0530

    Written More code

---
+++ static/js/script.js
@@ -0,0 +1,30 @@
+function nameNext(){
+    let nameBlock= document.querySelector(".full-name");
+    nameBlock.style.display="none";
+    let phoneBlock= document.querySelector(".phone-num");
+    phoneBlock.style.display="block";
+}
+function phoneNext(){
+    let phoneBlock= document.querySelector(".phone-num");
+    phoneBlock.style.display="none";
+    let emailBlock= document.querySelector(".email-id");
+    emailBlock.style.display="block";
+}
+function emailNext(){
+    let emailBlock= document.querySelector(".email-id");
+    emailBlock.style.display="none";
+    let zipBlock= document.querySelector(".zip-code");
+    zipBlock.style.display="block";
+}
+function zipNext(){
+    let zipBlock= document.querySelector(".zip-code");
+    zipBlock.style.display="none";
+    let bankBlock= document.querySelector(".bank-name");
+    bankBlock.style.display="block";
+}
+function bankNext(){
+    let bankBlock= document.querySelector(".bank-name");
+    bankBlock.style.display="none";
+    let accBlock= document.querySelector(".acc-num");
+    accBlock.style.display="block";
+}
