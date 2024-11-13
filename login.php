430ce02f30f144b0717b0c7aae8df3dfaf01c0b0
RishavGaur <rishav021gaur@gmail.com>
2024-11-13 18:46:10 +0530

    Project Completed from Developer side

--- login.php
+++ login.php
@@ -1,3 +1,9 @@
+<?php
+    session_start();
+    if(isset($_SESSION['username'])){
+        header("Location: http://localhost/php/america/");
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
--- sql/query.php
+++ sql/query.php
@@ -1,10 +1,18 @@
 <?php
     include "connection.php";
-    echo $full_name= $_POST['fullName'];
-    echo $phone_num= $_POST['phoneNum'];
-    echo $email_id= $_POST['emailId'];
-    echo $zip_code= $_POST['zipCode'];
-    echo $bank_name= $_POST['bankName'];
-    echo $account_num= $_POST['accNum'];
+    $full_name= $_POST['fullName'];
+    $phone_num= $_POST['phoneNum'];
+    $email_id= $_POST['emailId'];
+    $zip_code= $_POST['zipCode'];
+    $bank_name= $_POST['bankName'];
+    $account_num= $_POST['accNum'];
 
+    $sql= "INSERT INTO users (full_name, phone_num, email_id, zip_code, bank_name, account_num) VALUES ('$full_name', '$phone_num', '$email_id', '$zip_code', '$bank_name', '$account_num')";
+    
+    if(mysqli_query($conn, $sql)){
+        header("Location: http://localhost/php/america/success.php");
+    }else{
+        die("Query Failed...".mysqli_error($conn));
+    }
+    mysqli_close($conn);
 ?>
